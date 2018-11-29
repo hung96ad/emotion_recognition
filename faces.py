11daf78b8de266bce5967b19bb9f6ff237c10b8e
hungnt13 <hungnt13@topica.edu.vn>
2018-11-29 21:35:50 +0700

    first commit

---
+++ faces.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, make_response, request, abort, redirect, send_file
+import logging
+
+from processor import process_image 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect("http://tradersupport.club", code=302)
+
+@app.route('/classifyImage', methods=['POST'])
+def upload():
+    try:
+        image = request.files['image'].read()
+        file_name = process_image(image)
+        return send_file('result/%s.png'%file_name, mimetype='image/png')
+    except Exception as err:
+        logging.error('An error has occurred whilst processing the file: "{0}"'.format(err))
+        abort(400)
+
+@app.errorhandler(400)
+def bad_request(erro):
+    return make_response(jsonify({'error': 'We cannot process the file sent in the request.'}), 400)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Resource no found.'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8084)
---
+++ processor.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import logging
+
+import cv2
+from keras.models import load_model
+import numpy as np
+
+from utils.datasets import get_labels
+from utils.inference import detect_faces
+from utils.inference import draw_text
+from utils.inference import draw_bounding_box
+from utils.inference import apply_offsets
+from utils.inference import load_detection_model
+from utils.inference import load_image
+from utils.preprocessor import preprocess_input
+
+def process_image(image):
+    try:
+        # parameters for loading data and images
+        detection_model_path = './trained_models/detection_models/haarcascade_frontalface_default.xml'
+        emotion_model_path = './trained_models/emotion_models/fer2013.hdf5'
+        emotion_labels = get_labels('fer2013')
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        # hyper-parameters for bounding boxes shape
+        emotion_offsets = (20, 40)
+        emotion_offsets = (0, 0)
+
+        # loading models
+        face_detection = load_detection_model(detection_model_path)
+        emotion_classifier = load_model(emotion_model_path, compile=False)
+
+        # getting input model shapes for inference
+        emotion_target_size = emotion_classifier.input_shape[1:3]
+
+        # loading images
+        image_array = np.fromstring(image, np.uint8)
+        unchanged_image = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)
+
+        gray_image = cv2.cvtColor(unchanged_image, cv2.COLOR_BGR2GRAY)
+
+        faces = detect_faces(face_detection, gray_image)
+        for face_coordinates in faces:
+            x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets)
+            gray_face = gray_image[y1:y2, x1:x2]
+            try:
+                gray_face = cv2.resize(gray_face, (emotion_target_size))
+            except:
+                continue
+
+            gray_face = preprocess_input(gray_face, True)
+            gray_face = np.expand_dims(gray_face, 0)
+            gray_face = np.expand_dims(gray_face, -1)
+            emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+            emotion_text = emotion_labels[emotion_label_arg]
+
+            color = (255, 0, 0)
+
+            draw_bounding_box(face_coordinates, gray_face, color)
+            draw_text(face_coordinates, gray_face, emotion_text, color, 0, -50, 1, 2)
+    except Exception as err:
+        logging.error('Error in emotion processor: "{0}"'.format(err))
+
+    bgr_image = cv2.cvtColor(gray_face, cv2.COLOR_RGB2BGR)
+
+    dirname = 'result'
+    if not os.path.exists(dirname):
+        os.mkdir(dirname)
+
+    cv2.imwrite(os.path.join(dirname, 'predicted_image.png'), bgr_image)
+    return "predicted_image"
